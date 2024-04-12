857f74acb3932478b2f5e5f40237703d3f0a280b
banteg <4562643+banteg@users.noreply.github.com>
2024-04-13 02:44:24 +0400

    feat: pip blame

---
+++ pip_blame.py
@@ -0,0 +1,37 @@
+import importlib.metadata
+import sys
+
+import requests
+from packaging.requirements import Requirement
+from rich import print
+
+
+def main():
+    name = sys.argv[1]
+    resp = requests.get(f"https://pypi.org/pypi/{name}/json")
+    version = resp.json()["info"]["version"]
+    print(f"[bold green]{name} v{version}")
+
+    for dist in importlib.metadata.distributions():
+        homepage = dist.metadata.get("Home-page")
+        if dist.requires is None:
+            continue
+        for req in dist.requires:
+            req = Requirement(req)
+            if req.name != name:
+                continue
+            contains = req.specifier.contains(version)
+            if not contains:
+                color = "bold red"
+            elif "<" in str(req.specifier):
+                color = "yellow"
+            else:
+                color = "green"
+            msg = f"[{color}]{dist.name}[/]"
+            if not contains:
+                msg += f" {homepage}/issues/new" if homepage else ""
+            print(f"{msg}\n  {req}")
+
+
+if __name__ == "__main__":
+    main()
